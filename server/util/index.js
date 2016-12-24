4640f990c0694b1ca05153255ac361cecf1075ac
Tiago Azevedo <tabazevedo@gmail.com>
2016-12-24 00:20:48 +0000

    Functions!

--- server/util/index.js
+++ server/util/index.js
@@ -1,20 +1,12 @@
 import { red, yellow, blue, gray, white } from 'chalk';
 import fecha from 'fecha';
 
+const join = (...args) => args.join(' ');
+
 const timestamp = () => fecha.format(Date.now(), 'D/M/YYYY HH:mm:ss');
 
-const withTimestamp = (msg) => {
-  return `${gray(timestamp())} ${msg}`;
-}
+const withTimestamp = (msg) => join(gray(timestamp()), msg);
 
-export const error = (msg) => {
-  console.log(withTimestamp(red('[Error]') + ' ' + white(msg)));
-}
-
-export const log = (msg) => {
-  console.log(withTimestamp(blue('[Info]') + ' ' + white(msg)));
-}
-
-export const warn = (msg) => {
-  console.log(withTimestamp(yellow('[Warning]') + ' ' + white(msg)));
-}
+export const error = (msg) => console.log(withTimestamp(join(red('[Error]'), white(msg))));
+export const log = (msg) => console.log(withTimestamp(join(blue('[Info]'), white(msg))));
+export const warn = (msg) => console.log(withTimestamp(join(yellow('[Warning]'), white(msg))));
