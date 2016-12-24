a185cdf017da6f5051e512f9e48aadcbd27c0450
Tiago Azevedo <tabazevedo@gmail.com>
2016-12-24 14:49:30 +0000

    Client build

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,42 @@ module.exports = [
         {
           test: /\.js$/,
           loader: 'babel-loader',
+          query: {
+            presets: ['node7', 'react'],
+            env: {
+              development: {
+                presets: ['react-hmre'],
+              },
+            },
+          },
         },
       ],
     },
     target: 'node',
+  },
+  {
+    name: 'client',
+    context: __dirname,
+    entry: './client/index.js',
+    output: {
+      path: `${__dirname}/dist`,
+      filename: '[hash:5]-client.js',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['latest'],
+            env: {
+              development: {
+                presets: ['react-hmre'],
+              },
+            },
+          },
+        },
+      ],
+    },
   },
 ];
