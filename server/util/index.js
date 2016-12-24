391ecf1d269cc5a707b313357a2208c5c36f590e
Tiago Azevedo <tabazevedo@gmail.com>
2016-12-24 01:18:51 +0000

    Basic eslint config

--- server/util/index.js
+++ server/util/index.js
@@ -1,3 +1,4 @@
+/* eslint no-console: 0 */
 import { red, yellow, blue, gray, white } from 'chalk';
 import fecha from 'fecha';
 
@@ -5,8 +6,8 @@ const join = (...args) => args.join(' ');
 
 const timestamp = () => fecha.format(Date.now(), 'D/M/YYYY HH:mm:ss');
 
-const withTimestamp = (msg) => join(gray(timestamp()), msg);
+const withTimestamp = msg => join(gray(timestamp()), msg);
 
-export const error = (msg) => console.log(withTimestamp(join(red('[Error]'), white(msg))));
-export const log = (msg) => console.log(withTimestamp(join(blue('[Info]'), white(msg))));
-export const warn = (msg) => console.log(withTimestamp(join(yellow('[Warning]'), white(msg))));
+export const error = msg => console.log(withTimestamp(join(red('[Error]'), white(msg))));
+export const log = msg => console.log(withTimestamp(join(blue('[Info]'), white(msg))));
+export const warn = msg => console.log(withTimestamp(join(yellow('[Warning]'), white(msg))));
