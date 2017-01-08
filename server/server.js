8fbe7db6c5c04beb2e10291673ed36589393e9f1
Tiago Azevedo <tabazevedo@gmail.com>
2017-01-08 15:50:14 +0000

    Streaming support removed

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import infernoRouter from './middleware/inferno-router-koa';
 const server = new Koa();
 
 server.use(logger);
-server.use(infernoRouter(routes, { stream: true }));
+server.use(infernoRouter(routes));
 server.use(router.routes());
 server.use(router.allowedMethods());
 
