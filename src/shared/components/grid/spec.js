446eb84e21777c85e18f37038a51675f79bbf55f
Tiago Azevedo <tabazevedo@gmail.com>
2017-01-11 00:07:43 +0000

    Update some unit tests in grid

--- src/shared/components/grid/spec.js
+++ src/shared/components/grid/spec.js
@@ -1,31 +1,31 @@
 import Inferno from 'inferno';
 import { expect } from 'chai';
-import { render } from 'test/utils';
+import { render, selector } from 'test/utils';
 import { Col, Container, Row } from './index';
 import styles from './styles.css';
 
 describe('grid/container', () => {
   it('renders children', () => {
-    const tree = render(<Container>Hello, world!</Container>);
-    expect(tree.text()).to.equal('Hello, world!');
+    const $ = render(<Container>Hello, world!</Container>);
+    expect($.text()).to.equal('Hello, world!');
   });
 
   it('is fluid by default', () => {
-    const tree = render(<Container>Hello, world!</Container>);
-    expect(tree.attr('class')).to.contain(styles.fluid);
+    const $ = render(<Container>Hello, world!</Container>);
+    expect($(selector(styles.container)).attr('class')).to.contain(styles.fluid);
   });
 });
 
 describe('grid/col', () => {
   it('renders children', () => {
-    const tree = render(<Col>Hello, world!</Col>);
-    expect(tree.text()).to.equal('Hello, world!');
+    const $ = render(<Col>Hello, world!</Col>);
+    expect($.text()).to.equal('Hello, world!');
   });
 });
 
 describe('grid/row', () => {
   it('renders children', () => {
-    const tree = render(<Row>Hello, world!</Row>);
-    expect(tree.text()).to.equal('Hello, world!');
+    const $ = render(<Row>Hello, world!</Row>);
+    expect($.text()).to.equal('Hello, world!');
   });
 });
--- test/utils.js
+++ test/utils.js
@@ -2,5 +2,6 @@ import cheerio from 'cheerio';
 import { renderToStaticMarkup } from 'inferno-server';
 
 export const render = node => cheerio.load(renderToStaticMarkup(node));
+export const selector = (...styles) => styles.map(style => `.${style}`).join('');
 
 export default { render };
