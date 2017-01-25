2ef8f8aa15839061a0bc86ac5c3d561176089f9d
Tiago Azevedo <tabazevedo@gmail.com>
2017-01-25 00:13:03 +0000

    Add some type checking to config/validators

--- src/shared/util/config/index.js
+++ src/shared/util/config/index.js
@@ -1,26 +1,26 @@
-export default function createConfig() {
+export default function createConfig(validateAtRuntime: boolean = false) {
   const validators = new Map();
   const values = new Map();
 
   const conf = {
-    validateAtRuntime: false,
-    set: (key, value, validator) => {
+    validateAtRuntime,
+    set: (key: any, value: any, validator: func): void => {
       values.set(key, value);
       if (validator) validators.set(key, validator);
       conf.validate(key);
     },
-    get: (key) => {
+    get: (key: any): any => {
       if (conf.validateAtRuntime) conf.validate(key);
       return values.get(key);
     },
-    validate: (key) => {
+    validate: (key: any): void => {
       if (!validators.has(key)) return;
       const error = validators.get(key)(values.get(key));
       if (typeof error === 'string') conf.log(key, error);
       if (typeof error === 'boolean' && !error) conf.log(key);
     },
     logger: console.log, // eslint-disable-line no-console
-    log: (key, message) => conf.logger(`config error at '${key}'${(message ? `: ${message}` : '')}`),
+    log: (key: any, message: string) => conf.logger(`config error at '${key}'${(message ? `: ${message}` : '')}`),
   };
 
   return conf;
--- src/shared/util/validators/index.js
+++ src/shared/util/validators/index.js
@@ -1,6 +1,6 @@
 import curry from 'lodash/fp/curry';
 import isString from 'lodash/fp/isString';
 
-const validator = curry((message, valid) => valid || message);
+const validator = curry((message: string, valid: boolean) => valid || message);
 
 export { isString, validator };
